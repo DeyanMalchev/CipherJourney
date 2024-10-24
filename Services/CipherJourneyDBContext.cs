be7f1186ca69ec1a04f871166c97e90a883b71ad
Deian Malchev <dm.maineaz@gmail.com>
2024-10-24 10:41:52 +0300

    Started migrating the database. Put the tables in one DBConetxt

---
+++ Services/CipherJourneyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Experiments.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CipherJourney.Services
+{
+    public class CipherJourneyDBContext : DbContext
+    {
+        public CipherJourneyDBContext(DbContextOptions<CipherJourneyDBContext> options) : base(options) { 
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<User> UserPoints { get; set; }
+
+    }
+}
