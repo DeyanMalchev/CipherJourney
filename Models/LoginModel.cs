ab183d395f454dcaff2b5d6c782c03c4124f3a25
Deian Malchev <dm.maineaz@gmail.com>
2024-12-17 22:52:48 +0200

    Went back to 1 login model that accepts both username and email. Configured Login method and Login view page

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -4,6 +4,10 @@ namespace CipherJourney.Models
 {
     public class LoginModel
     {
+
+        [Required]
+        public string Info { get; set; }
+
         [Required]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
         public string Password { get; set; }
