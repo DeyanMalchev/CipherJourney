1a397c82f39865aa465b8be30fcf2f763218f8cb
Deian Malchev <dm.maineaz@gmail.com>
2025-04-22 18:48:41 +0300

    Tested data insertion through migration builder.

---
+++ Migrations/20250422073844_AddSentenceAndCipherData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CipherJourney.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSentenceAndCipherData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+            table: "Ciphers",
+            columns: new[] { "Cipher" },
+            values: new object[,]
+            {
+                    { "Caesar" }
+            });
+
+            migrationBuilder.InsertData(
+                table: "SentencesDaily",
+                columns: new[] { "Sentence" },
+                values: new object[,]
+                {
+                    { "The quick brown fox jumps over the lazy dog." },
+                    { "Knowledge is power!" },
+                    { "Encryption makes data secure!" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
